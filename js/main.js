f588bfb8c102d764f5b83e4c718603df56e35f8b
Stiven Aguilar <stivenaguilar10288@gmail.com>
2025-10-05 18:12:13 -0500

    Agregue estilos al responsive y el boton de menu en responsive

--- js/main.js
+++ js/main.js
@@ -23,3 +23,14 @@ document.getElementById("btnVerMas").addEventListener("click", function() {
   }
 });
 
+
+document.querySelectorAll('.navbar-collapse .nav-link').forEach(link => {
+  link.addEventListener('click', () => {
+    const navbar = document.querySelector('.navbar-collapse');
+    if (navbar.classList.contains('show')) {
+      const bsCollapse = new bootstrap.Collapse(navbar, {
+        toggle: true
+      });
+    }
+  });
+});
